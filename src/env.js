8624d26c088bdf27e4f795ef7105b5b6321f194a
hovgor <gor95hov@gmail.com>
2022-11-14 15:04:32 +0400

    make it better

--- src/env.js
+++ src/env.js
@@ -1,6 +1,5 @@
 // Описание переменных окружения смотри в Readme.
 const env = process.env;
-
 module.exports = {
     GLS_FRONTEND_GATE_HOST: env.GLS_FRONTEND_GATE_HOST || '0.0.0.0',
     GLS_FRONTEND_GATE_PORT: env.GLS_FRONTEND_GATE_PORT || 8080,
